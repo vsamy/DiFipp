3ce8bb3a4fba3195517ff6cc5096d5b0df056af8
Vincent Samy <vincent.samy@aist.go.jp>
2018-12-18 16:19:12 +0900

    Add band frequency check

--- tests/GenericFilterTests.cpp
+++ tests/GenericFilterTests.cpp
@@ -22,4 +22,6 @@ BOOST_AUTO_TEST_CASE(FILTER_FAILURES)
     BOOST_REQUIRE(mad.status() == fratio::FilterStatus::BAD_ORDER_SIZE);
     auto bfd = fratio::Butterworthd(0, 10, 100);
     BOOST_REQUIRE(bfd.status() == fratio::FilterStatus::BAD_ORDER_SIZE);
+    bfd = fratio::Butterworthd(5, 10, 100, 3);
+    BOOST_REQUIRE(bfd.status() == fratio::FilterStatus::BAD_BAND_FREQUENCY);
 }
